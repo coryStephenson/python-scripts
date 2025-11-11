ee499a74044605b55d31871c50b1f5d1b836ae7e
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-11 15:12:49 -0600

    Create negative_index_numbers.py

---
+++ python-crash-course/Chapter-3/Lists/negative_index_numbers.py
@@ -0,0 +1,7 @@
+# Python has a special syntax rule that allows for more convenient access
+# to the elements in the back of a list.
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
