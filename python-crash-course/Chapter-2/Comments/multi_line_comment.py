b917337b1d48704de45340539fd88b38d3ea7d05
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-11 14:21:44 -0600

    Created multi_line_comment.py

---
+++ python-crash-course/Chapter-2/Comments/multi_line_comment.py
@@ -0,0 +1,9 @@
+# Source for multi-line comment: https://learnxinyminutes.com/python/
+# Say hello to everyone.
+
+print("Hello Python people!")
+
+""" Multiline strings can be written
+    using three "s, and are often used
+    as documentation.
+"""
