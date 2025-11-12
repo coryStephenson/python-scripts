cae1e751dcfe077853eb8200414189d74ff5e37a
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-12 12:37:34 -0600

    Added print statements

--- python-crash-course/Chapter-3/Lists/greetings_by_name.py
+++ python-crash-course/Chapter-3/Lists/greetings_by_name.py
@@ -1,2 +1,17 @@
+names = ['Mike', 'Terrie', 'Joe', 'Lisa', 'Susie', 'Donnie']
+
 
 mike_greeting = f"Hello {names[0]}!"
+terrie_greeting = f"Hello {names[1]}!"
+joe_greeting = f"Hello {names[2]}!"
+lisa_greeting = f"Hello {names[3]}!"
+susie_greeting = f"Hello {names[4]}!"
+donnie_greeting = f"Hello {names[5]}!"
+
+
+print(mike_greeting)
+print(terrie_greeting)
+print(joe_greeting)
+print(lisa_greeting)
+print(susie_greeting)
+print(donnie_greeting)
