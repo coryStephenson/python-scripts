b0059dae82ca114797eb8628d48e690a53c27b28
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-21 14:56:25 -0600

    Create dinner_invitation.py

---
+++ python-crash-course/Chapter-3/Lists/Try-It-Yourself/dinner_invitation.py
@@ -0,0 +1,10 @@
+attendees = ['seth', 'katie', 'joe']
+
+greeting = attendees[-1]
+print(f"Hello {greeting.title()}, how are you doing? I cordially invite you to dinner.")
+
+greeting = attendees[-2]
+print(f"Hello {greeting.title()}, how are you doing? I cordially invite you to dinner.")
+
+greeting = attendees[-3]
+print(f"Hello {greeting.title()}, how are you doing? I cordially invite you to dinner.")
