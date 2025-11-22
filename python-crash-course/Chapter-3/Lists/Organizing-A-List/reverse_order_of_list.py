7f1dd1977ff1455b614609ba81f4edd2d1c6ed59
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-22 15:34:42 -0600

    Create reverse_order_of_list.py

---
+++ python-crash-course/Chapter-3/Lists/Organizing-A-List/reverse_order_of_list.py
@@ -0,0 +1,10 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+# Permanently reverse the order of a list using the reverse() method
+cars.reverse()
+print(cars)
+
+# Obtain the original order of a list by applying the method again
+cars.reverse()
+print(cars)
