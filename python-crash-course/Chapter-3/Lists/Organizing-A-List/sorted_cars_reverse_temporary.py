39d42dcda170c8765f05b95a98e917b75166dc21
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-22 15:23:13 -0600

    Create sorted_cars_reverse_temporary.py

---
+++ python-crash-course/Chapter-3/Lists/Organizing-A-List/sorted_cars_reverse_temporary.py
@@ -0,0 +1,11 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+# Temporarily sort the list in reverse alphabetical order
+print("\nHere is the sorted list:")
+print(sorted(cars, reverse = True))
+
+print("\nHere is the original list again:")
+print(cars)
