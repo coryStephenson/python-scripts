3fe48a180a6d3c63686849611d5a410a25b86652
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-22 15:53:32 -0600

    Create finding_length_of_list.py

---
+++ python-crash-course/Chapter-3/Lists/Organizing-A-List/finding_length_of_list.py
@@ -0,0 +1,6 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# The print() function is required to display the output of the len() function in a script
+# In interactive mode, len(cars) will suffice.
+
+print(len(cars))
