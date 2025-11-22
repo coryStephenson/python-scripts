bc7c6e6b71711193e087131352c2ae511eb9f917
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-22 15:17:13 -0600

    Create sorted_cars_temporary.py

---
+++ python-crash-course/Chapter-3/Lists/Organizing-A-List/sorted_cars_temporary.py
@@ -0,0 +1,11 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+# Temporarily sort the list in alphabetical order
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
