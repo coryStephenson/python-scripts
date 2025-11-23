f3f68467e2e5d8e25ec6232e288238a076008f5a
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-11-23 13:23:23 -0600

    Create convert_elevator_floors.py
    European elevator floors start with 0. Elevator floors in the US start with 1.

---
+++ convert_elevator_floors.py
@@ -0,0 +1,11 @@
+# Convert elevator floors
+inp = input('Europe floor?')
+
+""" If we want to read in a number
+    from the user, we must convert 
+    it from a string to a number
+    using a type conversion function
+"""
+
+usf = int(inp) + 1
+print('US floor', usf)
